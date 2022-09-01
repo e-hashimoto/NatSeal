de234488a459e14970d27b63274ad2e194232561
Exequiel <exequiel.hashimoto@outlook.com>
2022-09-01 15:04:46 -0700

    working on testing postman routes

--- app/api/location_routes.py
+++ app/api/location_routes.py
@@ -12,7 +12,8 @@ location_routes = Blueprint('locations', __name__)
 def edit_location(id):
     data = request.json
     location = Location.query.get(id)
-    location.coordinates = request.json['coordinate']
+    location.latitude = request.json['latitude']
+    location.longitude = request.json['longitude']
     location.description = request.json['description']
     db.session.commit()
     return location.to_dict()
@@ -20,7 +21,7 @@ def edit_location(id):
 # Get All Locations
 
 @location_routes.route('/')
-@login_required
+# @login_required
 def all_locations():
     locations = Location.query.all()
     data = [location.to_dict() for location in locations]
@@ -29,14 +30,14 @@ def all_locations():
 # Get a Single Location
 
 @location_routes.route('/<int:id>')
-@login_required
+# @login_required
 def one_location(id):
     location = Location.query.get(id)
     return location.to_dict()
 
 # Post a Single Location
 @location_routes.route('/', methods=['POST'])
-@login_required
+# @login_required
 def create_location():
     if "location" not in request.locations:
         return "No user_location key in request.locations"
@@ -59,7 +60,7 @@ def create_location():
 
 # Delete a Single Location
 @location_routes.route('/<int:id', methods=['DELETE'])
-@login_required
+# @login_required
 def delete_location(id):
     location = Location.query.get(id)
     db.session.delete(location)
