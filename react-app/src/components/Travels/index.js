7f21678ced0f5b7a424a0953ea9b882ec40389e6
Exequiel <exequiel.hashimoto@outlook.com>
2022-09-09 15:42:45 -0700

    travel page set

--- react-app/src/components/Travels/index.js
+++ react-app/src/components/Travels/index.js
@@ -1,2 +1,43 @@
 import { getAllTravels, getSingleTravel } from "../../store/travels";
-import Create
+import CreateTravelForm from "./CreateTravelForm";
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { NavLink, Redirect } from "react-router-dom";
+import "./travel.css";
+
+const Travels = () => {
+    const dispatch = useDispatch();
+    const travelsObject = useSelector((state) => state.travels);
+    const travels = Object.values(travelsObject);
+
+    const sessionUser = useSelector((state) => state.session.user);
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch("/api/users/");
+            const resData = await response.json();
+            setUsers(resData.users);
+        };
+        fetchData();
+    }, []);
+
+    const handleClick = (id) => {
+        dispatch(getSingleTravel(id));
+    };
+
+    if (!sessionUser) return <Redirect to="/login" />;
+
+    return (
+        <div className="travel-page">
+            <div className="column-container">
+                {travels.map((travel) => {
+                    return <div key={travel.id}>
+                        <NavLink exact to={`/travels/${travel.id}`} onClick={()=> handleClick(travel.id)}>{travel.name}</NavLink>
+                    </div>
+                })}
+            </div>
+        </div>
+    )
+}
